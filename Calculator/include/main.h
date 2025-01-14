966a56b4f64c90d1b64089d14c59d54774f19f23
Ashish <ashishmondal712223@gmail.com>
2025-01-14 14:00:28 +0530

    Intermediate Commit

--- Calculator/include/main.h
+++ Calculator/include/main.h
@@ -2,15 +2,43 @@
 #include <iostream>
 
 namespace Cal
-{
+{	
+
 	template<typename input>
-	void Printer(const input& value)
+	void ptrdel(input*& value)
+	{
+		if (value != nullptr)
+		{
+			delete value;
+			value = nullptr;
+		}
+		else
+		{
+
+		}
+	}
+
+	template<typename input>
+	void NPrinter(const input& value)
 	{
 		input* valuep = new input(value);
 		std::cout << *valuep << "\n";
 		
-		delete valuep;
-		valuep = nullptr;
+		ptrdel(valuep);
+	}
+
+	template<typename input1,typename input2>
+	void Menu(const input1& value1, const input2& value2)
+	{
+		input1* val1 = new input1(value1);
+		input2* val2 = new input2(value2);
+
+		std::cout << *val1 << *val2 << "\n";
+
+		ptrdel(val1);
+		ptrdel(val2);
+
 	}
 
 }
+
--- Calculator/src/main.cpp
+++ Calculator/src/main.cpp
@@ -6,6 +6,34 @@
 int main()
 {
 	// Top level welcome statement
-	Cal::Printer(std::string("  WELCOME TO THE CALCULATOR  "));
+	Cal::NPrinter(std::string("  WELCOME TO THE CALCULATOR  "));
+
+	Cal::NPrinter(std::string("    "));
+	Cal::NPrinter(std::string("    "));
+
+	Cal::NPrinter(std::string("     CHOOSE YOUR OPTION    "));
+	
+	Cal::NPrinter(std::string("    "));
+
+	Cal::Menu(std::string("     A. "),std::string("Add"));
+
+	Cal::NPrinter(std::string("    "));
+
+	Cal::Menu(std::string("     B. "),std::string("Subtract"));
+
+	Cal::NPrinter(std::string("    "));
+
+	Cal::Menu(std::string("     C. "),std::string("Multiply"));
+
+	Cal::NPrinter(std::string("    "));
+
+	Cal::Menu(std::string("     D. "),std::string("Divide"));
+
+	Cal::NPrinter(std::string("    "));
+
+	Cal::Menu(std::string("     E. "),std::string("Exit"));
+	
+	
+	
 
 }
